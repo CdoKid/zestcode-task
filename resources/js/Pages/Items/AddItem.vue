3ed9fe46674d6839ad1ee344a0f911b9742733e5
dev5 <reggiestain@gmail.com>
2023-11-09 12:09:27 +0200

    updated with add item component

---
+++ resources/js/Pages/Items/AddItem.vue
@@ -0,0 +1,83 @@
+<script setup>
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import { useForm } from '@inertiajs/vue3';
+import { ref } from 'vue';
+
+const nameInput = ref(null);
+const currentPasswordInput = ref(null);
+
+const form = useForm({
+    name: '',
+    desc: '',
+    price: '',
+});
+
+const addItem = () => {
+    form.put(route('item.add'), {
+        onFinish: () => form.reset(),
+    });
+};
+</script>
+
+<template>
+    <section>
+        <header>
+            <h2 class="text-lg font-medium text-gray-900">Add Item</h2>
+        </header>
+
+        <form @submit.prevent="addItem" class="mt-6 space-y-6">
+            <div>
+                <InputLabel for="name" value="Name" />
+
+                <TextInput
+                    id="name"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.name"
+                    required
+                    autofocus
+                    autocomplete="name"
+                />
+
+                <InputError class="mt-2" :message="form.errors.name" />
+            </div>
+            <div>
+                <InputLabel for="desc" value="Description" />
+
+                <TextInput
+                    id="desc"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.desc"
+                    required
+                    autofocus
+                    autocomplete="name"
+                />
+
+                <InputError class="mt-2" :message="form.errors.desc" />
+            </div>
+            <div>
+                <InputLabel for="price" value="Price" />
+
+                <TextInput
+                    id="price"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.price"
+                    required
+                    autofocus
+                    autocomplete="name"
+                />
+
+                <InputError class="mt-2" :message="form.errors.price" />
+            </div>
+
+            <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Save
+            </PrimaryButton>
+        </form>
+    </section>
+</template>
---
+++ resources/js/Pages/Items/ItemsList.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+<template>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900">Items List</h2>
+    </header>   
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Item name
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Description
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Price
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Apple MacBook Pro 17"
+                    </th>
+                    <td class="px-6 py-4">
+                        Silver
+                    </td>
+                    <td class="px-6 py-4">
+                        $2999
+                    </td>
+                    <td class="px-6 py-4">
+                        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">delete</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</template>
